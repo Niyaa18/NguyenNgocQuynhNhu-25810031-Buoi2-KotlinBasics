eb3b838363b373019803599b7e4430316af54928
Niyaa18 <nhunnq188@gmail.com>
2026-09-24 16:57:30 +0700

    Buoi02 Bai02 gioi thieu ban than

---
+++ Bai02_GioiThieuBanThan.kt
@@ -0,0 +1,12 @@
+// Nguyễn Ngọc Quỳnh Như - MSSV: 25810031
+
+fun main() {
+    val hoTen = "Nguyễn Ngọc Quỳnh Như"
+    val namSinh = 2004
+    val namHienTai = 2026
+
+    val tuoi = namHienTai - namSinh
+
+    // Dung string template $
+    println("Xin chao, toi ten la $hoTen, nam nay toi $tuoi tuoi.")
+}
